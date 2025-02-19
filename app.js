b42b3113a667f41c0c03f124e517bca8d985050c
aramval <annaramosvalldeperez@gmail.com>
2025-02-19 17:44:46 +0100

    Setup y Ref

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const app = createApp({
     template:`
     <h1>Hola Mundo</h1>
     <p>Desde app.js</p>`
+    , 
+
+    setup(){
+        const nuntius = ref("I'm Batman");
+        setTimeout(() => {
+            nuntius.value = "Soy Goku";
+            console.log(nuntius);
+        }, 1000)
+        return {
+            nuntius,
+            auctor,
+        }
+    }
 });
 
 app.mount('#myApp')
